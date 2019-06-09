d19d4ec01043a3662d38180503510ce37d7bbf70
Matej Jellus <juffalow@juffalow.com>
2019-06-09 18:10:43 +0200

    Add load more functionality

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,13 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 import SEO from 'components/SEO';
 import Quote from 'components/Quote';
 import QuotesContainer from 'containers/QuotesContainer';
 
-const Home = ({ quotes }) => (
-  <SEO title="Quotes" description="List of Quotes with Authors implemented in React + Relay.">
+const Home = ({ quotes, hasMore, loadMore }) => (
+  <SEO title="Quotes" description="List of Quotes with Authors implemented in React and Relay.">
     <Container style={{ marginTop: 20, marginBottom: 20 }}>
       <Row>
         <Col>
@@ -22,6 +23,15 @@ const Home = ({ quotes }) => (
             </Col>
           ))
         }
+      </Row>
+      <Row>
+        <Col className="text-center">
+          {
+            hasMore ? (
+              <Button onClick={loadMore}>Load more</Button>
+            ) : null
+          }
+        </Col>
       </Row>
     </Container>
   </SEO>
@@ -39,8 +49,18 @@ export default () => (
           params,
         );
       }}
-      isLoading={relay.isLoading}
-      hasMore={relay.hasMore}
-      loadMore={relay.loadMore} />
+      hasMore={relay.hasMore()}
+      loadMore={() => {
+        if (!relay.hasMore() || relay.isLoading()) {
+          return;
+        }
+
+        relay.loadMore(
+          9,
+          error => {
+            console.log(error);
+          },
+        );
+      }} />
   )} />
 );
