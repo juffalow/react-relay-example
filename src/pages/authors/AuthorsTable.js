34e271b680b769bdbd762c8308152c66a5c43a62
Matej Jellus <matej.jellus@itera.no>
2019-09-11 15:14:57 +0200

    Split authors table components

--- src/pages/authors/AuthorsTable.js
+++ src/pages/authors/AuthorsTable.js
@@ -2,24 +2,7 @@ import React from 'react'
 import Table from 'react-bootstrap/Table';
 import { createFragmentContainer } from 'react-relay';
 import graphql from 'babel-plugin-relay/macro';
-
-const AuthorRow = ({ author }) => (
-  <>
-    <td>{author._id}</td>
-    <td>{author.firstName}</td>
-    <td>{author.lastName}</td>
-  </>
-);
-
-const AuthorRowFragmentContainer = createFragmentContainer(AuthorRow, {
-  author: graphql`
-     fragment AuthorsTable_author on Author {
-        _id
-        firstName
-        lastName
-     }
-  `
-});
+import AuthorRow from './AuthorRow';
 
 const AuthorsTable = ({ authors, ...rest }) => (
   <Table striped bordered hover>
@@ -34,7 +17,7 @@ const AuthorsTable = ({ authors, ...rest }) => (
       {
         authors.map(author => (
           <tr key={author.id}>
-            <AuthorRowFragmentContainer author={author} />
+            <AuthorRow author={author} />
           </tr>
         ))
       }
@@ -46,7 +29,7 @@ const AuthorsTableFragmentContainer = createFragmentContainer(AuthorsTable, {
   authors: graphql`
     fragment AuthorsTable_authors on Author @relay(plural: true) {
       id
-      ...AuthorsTable_author
+      ...AuthorRow_author
     }
   `
 });
