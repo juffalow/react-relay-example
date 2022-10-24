449dae57a8bfe0bd14708beba8dee6866765c5bf
Matej Jellus <juffalow@juffalow.com>
2022-10-24 20:22:04 +0200

    Add html description, fix typo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,12 +6,12 @@ import SEO from '../components/SEO';
 import HomeContainer from './home/HomeContainer';
 
 const Home: React.FC = () => (
-  <SEO title="Quotes" description="">
+  <SEO title="Quotes" description="Example project how to use React, Relay and TypeScript">
     <Container>
       <Row>
         <Col>
-          <h1>Quotes - React &amp; Relay examole project</h1>
-          <p className="lead">The whole page is created with <a href="https://create-react-app.dev/" rel="noopener noreferrer" target="_blank">create-react-app</a> without eject. It uses newest version of <a href="https://reactjs.org/" rel="noopener noreferrer" target="_blank">React</a> (<i>18.0.0</i>) and <a href="https://relay.dev/" rel="noopener noreferrer" target="_blank">Relay</a> (<i>13.0.0</i>) that is now available (<i>19.4.2022</i>) and everything is coded in <a href="https://www.typescriptlang.org/" rel="noopener noreferrer" target="_blank">TypeScript</a>. You can find here how to use <code>useLazyLoadQuery</code>, <code>useFragment</code> and <code>usePaginationFragment</code>. Backend is available in <a href="https://github.com/juffalow/slim-graphql-eloquent-example" rel="noopener noreferrer" target="_blank">PHP</a> and <a href="https://github.com/juffalow/express-graphql-example" rel="noopener noreferrer" target="_blank">NodeJS</a>.</p>
+          <h1>Quotes - React &amp; Relay example project</h1>
+          <p className="lead">The whole page is created with <a href="https://create-react-app.dev/" rel="noopener noreferrer" target="_blank">create-react-app</a> without eject. It uses newest version of <a href="https://reactjs.org/" rel="noopener noreferrer" target="_blank">React</a> (<i>18.0.0</i>) and <a href="https://relay.dev/" rel="noopener noreferrer" target="_blank">Relay</a> (<i>14.0.0</i>) that is now available (<i>8.6.2022</i>) and everything is coded in <a href="https://www.typescriptlang.org/" rel="noopener noreferrer" target="_blank">TypeScript</a>. You can find here how to use <code>useLazyLoadQuery</code>, <code>useFragment</code> and <code>usePaginationFragment</code>. Backend is available in <a href="https://github.com/juffalow/slim-graphql-eloquent-example" rel="noopener noreferrer" target="_blank">PHP</a> and <a href="https://github.com/juffalow/express-graphql-example" rel="noopener noreferrer" target="_blank">NodeJS</a>.</p>
           <p>This project was created because of lot of hours spent searching, trying and debugging. There are several articles and projects about Relay, but those are mostly using older versions, or it is just a small sample of a code without other major parts. Here is a complete web application with everything you need to start using React, Relay and GraphQL.</p>
           <HomeContainer />
           <h2>Want to contribute?</h2>
