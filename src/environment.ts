4a932e38588932952bd383d37751b95367584994
Matej Jellus <juffalow@juffalow.com>
2025-03-31 16:01:29 +0200

    Use new GraphQL endpoint

--- src/environment.ts
+++ src/environment.ts
@@ -8,10 +8,11 @@ import { RequestParameters } from 'relay-runtime/lib/util/RelayConcreteNode';
 import { Variables } from 'relay-runtime/lib/util/RelayRuntimeTypes';
 
 async function fetchGraphQL(params: RequestParameters, variables: Variables) {
-  const response = await fetch(`https://graphql.juffalow.com/graphql`, {
+  const response = await fetch(process.env.REACT_APP_GRAPHQL_URL as string, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
+      'x-api-key': process.env.REACT_APP_API_KEY as string,
     },
     body: JSON.stringify({
       query: params.text,
